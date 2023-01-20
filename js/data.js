ee173766c45a99bf1333a6371b81c70ed731f9d7
ericyork <eyork@clarkson.edu>
2023-01-20 15:12:43 -0500

    adds styles and js support

---
+++ js/data.js
@@ -0,0 +1,18 @@
+const allQuotes = [
+  {
+    "text": "Lorem ipsum dolor sit amet escrepitur reddit, dang.",
+    "author": "some Roman, probably",
+    "source": "The Wall",
+    "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    "date": "06/08/2006",
+    "tag": "default"
+  },
+  {
+    "text": "Dang, reddit escrepitur amet sit dolor ipsum lorem, sei.",
+    "author": "an erstwhile joker",
+    "source": "Your Mom",
+    "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    "date": "03/25/2009",
+    "tag": "default"
+  }
+]
---
+++ js/page.js
@@ -0,0 +1,10 @@
+// const qTexts = document.getElementsByTagName('blockquote'); 
+// const qAuthors =  document.getElementsByTagName('figcaption'); 
+// const qSources =  document.getElementsByTagName('cite');
+// const qDates = document.getElementsByClassName('dates');
+// const qTags =  document.getElementsByClassName('tags');
+
+
+window.addEventListener('load', () => {
+    console.log("waiting...")
+});
