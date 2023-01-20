8c66662c69af142cfb20cc0c888f366bc7110d5b
ericyork <eyork@clarkson.edu>
2023-01-20 16:57:55 -0500

    basic factory structure

--- js/page.js
+++ js/page.js
@@ -5,6 +5,15 @@
 // const qTags =  document.getElementsByClassName('tags');
 
 
-window.addEventListener('load', () => {
-    console.log("waiting...")
-});
+// window.addEventListener('load', () => {
+//     console.log("waiting...")
+// });
+
+const quoteFactory = (text, author, source, path, date, tag) => {
+  const giveQuote = () => console.log( text + ', ' + author );
+  return { text, author, source, path, date, tag, giveQuote };
+};
+
+const firstOne = quoteFactory(allQuotes[1].text, allQuotes[1].author);
+
+firstOne.giveQuote();
